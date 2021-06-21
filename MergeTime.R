e0f4922a6ed237388db94d5ca4bc3319350a7599
joanbout <joanthesmart@gmail.com>
2021-06-21 20:50:03 +0300

    Update MergeTime.R

--- MergeTime.R
+++ MergeTime.R
@@ -1,13 +1,13 @@
 #' MergeTime
 #'
-#' H συνάρτηση δέχεται ως είσοδο ένα dataframe με τα δεδομένα των features, 
-#' όπως αυτά δομήθηκαν από την LoadDataset function. Δημιουργεί ένα νέο columnn
-#' "Time" στο dataframe, που αποτελεί την ένωση των τριών πεδίων σχετικών με 
-#' χρονική στιγμή ("Hour","Minute", "Second"), και διαγράφει τα παραπάνω πεδία 
-#' από το dataframe.
+#' The function accepts as input a dataframe with the feature data, 
+#' as structured by the LoadDataset function. Creates a new column
+#' "Time" in the dataframe, which is the union of the three fields related to
+# 'time ("Hour", "Minute", "Second"), and deletes the above fields
+# 'from the dataframe.
 #' 
-#' Θα εφαρμοστεί μέσω της lapply(SubjectList, MergeTime)
-#' όπου SubjectList η list των dataframes που δίνει η LoadDataset
+#' Will be implemented via lapply (SubjectList, MergeTime)
+#' where SubjectList is the list of dataframes provided by LoadDataset
 #' 
 MergeTime <-function(Subject) {
     
